4a6f73c3794ad6d6e73671a1ffac154cd3fbde0e
Hasan Sabri <hsabri2007@gmail.com>
2026-01-07 22:33:14 +0000

    fixed accidental commits on wrong remote

--- src/main.cxx
+++
@@ -1,9 +0,0 @@
-#include "ast.hxx"
-#include "token.hxx"
-
-int main(void)
-{
-	Token::Stream stream(R"(C:\Users\User\Documents\c\prose\tests\test.prose)");
-
-	AST::Root root = AST::Generate(stream);
-}
