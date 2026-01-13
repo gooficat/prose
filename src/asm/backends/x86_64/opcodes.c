ba4f661cc6f4ddef028af01dd4365d36b507d339
Hasan Sabri <hsabri2007@gmail.com>
2026-01-13 22:18:59 +0000

    x64 instruction struct

--- src/asm/backends/x86_64/opcodes.c
+++ src/asm/backends/x86_64/opcodes.c
@@ -1,6 +1,7 @@
 #include "opcodes.h"
 
 x64_mnem_t mnems[] = {
+	{ "add", 0, 6 },
 	{ "movq", 0, 0 },
 	{ "ret", 0, 0 },
 };
@@ -12,10 +13,25 @@ x64_opcode_t opcodes[] = {
 uint16_t num_opcodes = sizeof( opcodes ) / sizeof( opcodes[ 0 ] );
 
 x64_reg_t regs[] = {
-	{ "rax", X64_REG_Q | X64_R_REG, 0x00 },
-	{ "rcx", X64_REG_Q | X64_R_REG, 0x01 },
-	{ "rdx", X64_REG_Q | X64_R_REG, 0x02 },
-	{ "rbx", X64_REG_Q | X64_R_REG, 0x03 },
+	{ "rax", X64_REG_Q, 0x00 },
+	{ "rcx", X64_REG_Q, 0x01 },
+	{ "rdx", X64_REG_Q, 0x02 },
+	{ "rbx", X64_REG_Q, 0x03 },
+
+	{ "rsp", X64_REG_Q, 0x04 },
+	{ "rbp", X64_REG_Q, 0x05 },
+	{ "rsi", X64_REG_Q, 0x06 },
+	{ "rdi", X64_REG_Q, 0x07 },
+
+	{ "r8", X64_REG_Q | X64_R_REG, 0x00 },
+	{ "r9", X64_REG_Q | X64_R_REG, 0x01 },
+	{ "r10", X64_REG_Q | X64_R_REG, 0x02 },
+	{ "r11", X64_REG_Q | X64_R_REG, 0x03 },
+
+	{ "r12", X64_REG_Q | X64_R_REG, 0x04 },
+	{ "r13", X64_REG_Q | X64_R_REG, 0x05 },
+	{ "r14", X64_REG_Q | X64_R_REG, 0x06 },
+	{ "r15", X64_REG_Q | X64_R_REG, 0x07 },
 
 };
 uint8_t num_regs = sizeof( regs ) / sizeof( regs[ 0 ] );
--- src/asm/backends/x86_64/opcodes.h
+++ src/asm/backends/x86_64/opcodes.h
@@ -7,11 +7,12 @@
 
 typedef enum : uint8_t
 {
-	X64_REG_B,
-	X64_REG_W,
-	X64_REG_D,
-	X64_REG_Q,
-	X64_R_REG = 1 << 7,	 // expanded r-prefix registers (r8-r15)
+	X64_REG_B,			 // byte
+	X64_REG_W,			 // word
+	X64_REG_D,			 // dword
+	X64_REG_Q,			 // qword
+	X64_REG_S,			 // seg
+	X64_R_REG = 1 << 7,	 // expanded r-prefix registers (r8-r15) (bitmasked)
 } x64_reg_type_t;
 
 typedef struct
@@ -21,10 +22,11 @@ typedef struct
 	uint8_t code;
 } x64_reg_t;
 
-typedef enum : uint8_t
-{
-	FLDS_NO,
-} x64_flds_t;
+// typedef enum : uint8_t
+// {
+// 	FLDS_NO,
+// 	//
+// } x64_flds_t;
 
 typedef struct
 {
@@ -37,35 +39,85 @@ typedef enum : uint8_t
 {
 	X64_N_ARG,
 	X64_ARG_A,
+	X64_ARG_BA,
+	X64_ARG_BB,
+	X64_ARG_BD,
+	X64_ARG_C,
+	X64_ARG_D,
+	X64_ARG_E,
+	X64_ARG_ES,
+	X64_ARG_EST,
+	X64_ARG_F,
+	X64_ARG_G,
+	X64_ARG_H,
+	X64_ARG_I,
+	X64_ARG_J,
+	X64_ARG_M,
+	X64_ARG_N,
+	X64_ARG_O,
+	X64_ARG_P,
+	X64_ARG_Q,
+	X64_ARG_R,
+	X64_ARG_S,
+	X64_ARG_SC,
+	X64_ARG_T,
+	X64_ARG_U,
+	X64_ARG_V,
+	X64_ARG_W,
+	X64_ARG_X,
+	X64_ARG_Y,
+	X64_ARG_Z,
 } x64_arg_profile_t;
+
+/*Wordle 1669 X/6
+
+⬛🟩⬛⬛⬛
+⬛🟩⬛⬛⬛
+🟨🟩⬛⬛⬛
+⬛🟩🟩🟩🟩
+⬛🟩🟩🟩🟩
+⬛🟩🟩🟩🟩*/
 
 typedef enum : uint8_t
 {
-	PROC_X64,
+	PROC_00,
+	PROC_01,
+	PROC_02,
+	PROC_03,
+	PROC_04,
+	PROC_P1,
+	PROC_PX,
+	PROC_PP,
+	PROC_P2,
+	PROC_P3,
+	PROC_P4,
+	PROC_C1,
+	PROC_C2,
+	PROC_C7,
+	PROC_IT,
 } proc_86_t;
 
 typedef enum : uint8_t
 {
-	OPMODE_N,
+	OPMODE_R,
+	OPMODE_P,
+	OPMODE_E,
+	OPMODE_S,
 } mode_86_t;
-typedef enum : uint8_t
-{
-	RING_NO,
-	RING_Z0,
-	RING_F1,
-	RING_F2,
-} ring_86_t;
 
 typedef enum : uint8_t
 {
-	LOCK_X,
+	LOCK_L,
+	LOCK_S,
+	LOCK_p,
+	LOCK_P,
 } lock_fpup_t;
 
-typedef enum : uint8_t
-{
-	GRP_GEN,
-	GRP_ART,
-} grp_86_t;
+// typedef enum : uint8_t
+// {
+// 	GRP_GEN,
+// 	GRP_ART,
+// } grp_86_t;
 
 // #define NUM_GRP 3
 
@@ -76,11 +128,10 @@ typedef struct
 	uint8_t po;
 	bool has_so;
 	uint8_t so;
-	x64_flds_t flds;
+	// x64_flds_t flds;
 	uint8_t o;
 	proc_86_t proc;
 	mode_86_t mode;
-	ring_86_t ring;
 	lock_fpup_t lock_fpup;
 	x64_arg_profile_t args[ X64_ARG_MAX ];
 	// grp_86_t grp[ NUM_GRP ];
